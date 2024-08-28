f3f01e01276bd2ccee308170b54374884060c0e7
Abhay <abhayrajsingh@gmail.com>
2024-08-28 21:51:42 +0530

    Q1

---
+++ Q1/Test/bajaj/src/main/java/org/sample/bajaj/App.java
@@ -0,0 +1,13 @@
+package org.sample.bajaj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
